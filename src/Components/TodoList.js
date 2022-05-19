48e2c8f232734fae8510d2e039ddfa9991222699
qurrotaainida <qurrotaainida250701@gmail.com>
2022-05-19 23:01:50 +0700

    Step 2 done

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from './Header'
 import TodoItem from './TodoItem'
+import Footer from './Footer'
 
 const Todolist = (props) => {
 
     return (
         <div className="todo-list">
-        <Header title={props.title}/>
+         <Header title={props.title.toUpperCase()}  addNewItem={props.addNewItem}/>
         <ul className="list-group list-group-flush">
             {props.items.map(item => (
             <li className="list-group-item">
@@ -14,6 +15,7 @@ const Todolist = (props) => {
             </li>
             ))}
         </ul>
+        <Footer items={props.items} />
         </div>
     )
 }
